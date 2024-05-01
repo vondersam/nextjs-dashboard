48d8be021593c26aa16cc0a72d619b288394f756
Samuel Rodriguez <samuelyret@gmail.com>
2024-05-01 15:55:47 +0200

    Add module styles.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
@@ -12,7 +13,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          <div className={styles.shape} />
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
@@ -43,7 +44,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
       </div>
